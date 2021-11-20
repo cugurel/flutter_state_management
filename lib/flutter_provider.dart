765b704e7ecb6d1dddfd46a71dca0c3f7f5dcc3a
Çağrı Uğurel <cugurel7@gmail.com>
2021-11-20 22:40:07 +0300

    created provider screens

---
+++ lib/flutter_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterProvider extends StatelessWidget {
+  const FlutterProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'flutter_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("State Management"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 40,
+              child: RaisedButton(
+                child: Text("Provider"),
+                  onPressed: (){
+                   // Navigator.of(context).push(MaterialPageRoute(builder: (context) => FlutterStateProvider()));
+              }),
+            ),
+
+            SizedBox(
+              height: 5,
+            ),
+            SizedBox(
+              width: 200,
+              height: 40,
+              child: RaisedButton(
+                  child: Text("State Provider"),
+                  onPressed: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => FlutterProvider()));
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
