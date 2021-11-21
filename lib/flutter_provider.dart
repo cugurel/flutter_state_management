ccd49fbc3158bb5ecf7fb092a560031f50a8dce9
Çağrı Uğurel <cugurel7@gmail.com>
2021-11-21 11:37:36 +0300

    commit

--- lib/flutter_provider.dart
+++ lib/flutter_provider.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FlutterProvider extends StatelessWidget {
   const FlutterProvider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ProviderScope(child: Page());
+  }
+}
+
+final numberProvider = Provider<int>((ref){
+  return 42;
+});
+
+class Page extends ConsumerWidget {
+  const Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final number = watch(numberProvider);
+    return Scaffold(
+      body: Center(
+        child: Text(number.toString()),
+      ),
+    );
   }
 }
