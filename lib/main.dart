d01c55de2e990844d00917546cc6ba867238827e
Çağrı Uğurel <cugurel7@gmail.com>
2021-11-21 22:55:40 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'flutter_provider.dart';
-import 'flutter_state_provider.dart';
+void main() => runApp(ProviderScope(child: MyApp()));
 
-void main() {
-  runApp(ProviderScope(child: MyApp()));
+class IncrementNotifier extends ChangeNotifier{
+  int _value=0;
+  int get value => _value;
+
+  void increment(){
+    _value += 1;
+    notifyListeners();
+  }
 }
 
-final numberProvider = StateProvider<int>((ref){
-  return 42;
-});
+final greetingProvider = Provider((ref) => 'Hello Riverpod');
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-//smartgit trial
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("State Management"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 200,
-              height: 40,
-              child: RaisedButton(
-                child: Text("Provider"),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => FlutterProvider()));
-              }),
+        title: 'Riverpd Tutorial',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Riverpod"),
+          ),
+          body: Center(
+            child: Consumer(
+              builder: (context, watch, child) {
+                final greeting = watch(greetingProvider);
+                return Text(greeting);
+              },
             ),
-            SizedBox(
-              height: 5,
-            ),
-            SizedBox(
-              width: 200,
-              height: 40,
-              child: RaisedButton(
-                  child: Text("State Provider"),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => FlutterStateProvider()));
-                  }),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-
-            //smartgit yorumu
-            SizedBox(
-              width: 200,
-              height: 40,
-              child: RaisedButton(
-                  child: Text("State Provider"),
-                  onPressed: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => FlutterStateProvider()));
-                  }),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
